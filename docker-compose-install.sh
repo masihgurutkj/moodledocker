e665511ca850b7bf3533dd67580f88609530d667
masihgurutkj <49093218+masihgurutkj@users.noreply.github.com>
2023-05-14 22:56:19 +0700

    Update docker-compose-install.sh

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-apt update && 
-apt upgrade -y &&
-curl -fsSL https://get.docker.com -o get-docker.sh &&
-sudo sh get-docker.sh &&
-curl -SL https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose &&
-chmod +x /usr/local/bin/docker-compose &&
+apt update && \
+apt upgrade -y && \
+curl -fsSL https://get.docker.com -o get-docker.sh \&& sudo sh get-docker.sh && \
+curl -SL https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose && \
+chmod +x /usr/local/bin/docker-compose && \
 apt-get install docker-compose-plugin
